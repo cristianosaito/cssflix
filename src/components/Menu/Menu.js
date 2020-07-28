0ed58c0522523b8191adfd777e9b5854211552ba
cristianosaito <cristiano.saito@hotmail.com>
2020-07-28 18:49:48 -0300

    day2

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,6 @@
 import React from "react"
+import {Link} from 'react-router-dom';
+
 import logo from "../../assets/img/logo.png"
 import "./Menu.css"
 import Button from "../Button/Button"
@@ -6,10 +8,10 @@ import Button from "../Button/Button"
 function Menu(){
     return(
         <nav className="Menu">
-            <a href="/">
-            <img className="Logo" src={logo} alt="logo"/>
-            </a>
-            <Button as="a" href="/">
+            <Link to="/">
+                <img className="Logo" src={logo} alt="logo"/>
+            </Link>
+            <Button as={Link} to="/cadastro/video">
                 novo video
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/home/App';
+import CadastroVideo from './pages/cadastro/Video/video'
+import CadastroCategoria from './pages/cadastro/Categoria/categoria'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+      <Route path="/" component={App} exact/>
+      <Route component={()=>(<div>"Pagina 404"</div>)}/>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
